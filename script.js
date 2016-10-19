1be43e8f88969899a02b70157537b7c47300d8b1
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-19 20:20:33 +0000

    cambios estructura inicial

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var iaas = function(){
+    
+    var node_ssh = require('node-ssh');
+    var ssh = new node_ssh();
+    var pck = require("./package.json");
+    
+    ssh.connect({
+      host: pck.iaas.ip,
+      username: pck.iaas.user
+    }).then(function() {
+      ssh.execCommand(pck.iaas.command).then(function(result) {
+        console.log('STDOUT: ' + result.stdout);
+        console.log('STDERR: ' + result.stderr);
+      });
+    });
+};
+module.exports.iaas = iaas();
+
