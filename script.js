d633b2a33e83748c728ece6ee5b5cf2de7cd2ac9
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-20 16:36:15 +0000

    cambios no relevantes

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 "use strict";
 
-var iaas = () => {
-  var pck = require("./package.json");  
-  var SSH = require('simple-ssh');
-   
-  var ssh = new SSH({
-      host: pck.iaas.ip,
-      user: pck.iaas.user,
-      agent: process.env.SSH_AUTH_SOCK,
-      agentForward: true
-  });
-   
-  ssh.exec(pck.iaas.command, {
-      out: function(stdout) {
-          console.log(stdout);
-      }
-  }).start();
+module.exports = {
+  
+  initialize: () => {
+    console.log("hola");
+  },
+  
+  iaas: () => {
+    var pck = require("./package.json");  
+    var SSH = require('simple-ssh');
+     
+    var ssh = new SSH({
+        host: pck.iaas.ip,
+        user: pck.iaas.user,
+        agent: process.env.SSH_AUTH_SOCK,
+        agentForward: true
+    });
+     
+    ssh.exec(pck.iaas.command, {
+        out: function(stdout) {
+            console.log(stdout);
+        }
+    }).start();
+  }
+  
+  
 };
 
-/*
-var iaas = () => {
-    
-    var node_ssh = require("node-ssh");
-    var ssh = new node_ssh();
-    var pck = require("./package.json");
-    
-    ssh.connect({
-      host: pck.iaas.ip,
-      username: pck.iaas.user,
-      privateKey: '/home/jose/.ssh/iaas'
-    }).then(function() {
-      ssh.execCommand(pck.iaas.command).then(function(result) {
-        console.log('STDOUT: ' + result.stdout);
-        console.log('STDERR: ' + result.stderr);
-      });
-    });
-};
-*/
-module.exports.iaas = iaas;
 
