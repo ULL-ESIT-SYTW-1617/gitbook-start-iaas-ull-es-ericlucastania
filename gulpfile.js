5fb5b07a804b89640329b509383c6c0e45ded27a
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-27 21:33:36 +0000

    no relevants changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");  
 var shell = require('gulp-shell');
     
-gulp.task('deploy-iaas',shell.task([
-  'var iaas = require ("gitbook-start-plugin-iaas-ull-es-ericlucastania")',
-  'iaas.deploy()'
-]));
+gulp.task('deploy-iaas',function(){
+  var iaas = require ("gitbook-start-plugin-iaas-ull-es-ericlucastania");
+  iaas.deploy();
+});
