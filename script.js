92582e06671703e2a88729a3c795567cf4d0dcc0
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-27 22:29:43 +0000

    no relevants changes

--- script.js
+++ script.js
@@ -9,15 +9,11 @@ module.exports = {
     var reg =/deploy-iaas/gi;
     var ruta = path.join(__dirname,'gulpfile.js');
     var fs = require('fs-extra');
-    console.log(reg +  " expresion");
+
     
     fs.readFile(direct + 'gulpfile.js',"utf-8", (err, data) => {
-      console.log("entra");
       if (err) throw err;
-      console.log("data vale " + data);
-      console.log(data.match(reg) + " true or false");
       if(data.match(reg) == null){
-        console.log("entra");
         
         fs.readFile(ruta, (err, data) => {
           if (err) throw err;
@@ -30,7 +26,7 @@ module.exports = {
         
       }
     });
-    /*
+    
     require('shelljs/global');
     var pck = require("./package.json");
     try {
@@ -43,7 +39,7 @@ module.exports = {
     console.log("Introduzca la clave para configurar la clave authorized_keys \n");
     exec("ssh-copy-id -i iaas " + pck.iaas.user + "@" + pck.iaas.ip);
     console.log("Clave añadida al fichero authorized_keys\n");
-    exec("mv iaas ~/.ssh; mv iaas.pub ~/.ssh");*/
+    exec("mv iaas ~/.ssh; mv iaas.pub ~/.ssh");
     
   },
 
