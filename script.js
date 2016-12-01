8807564901a6dcbda1219382e7f067e5221216a6
alu0100785265 <alu0100785265@ull.edu.es>
2016-12-01 13:25:51 +0000

    no relevants changes

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ module.exports = {
       agentForward: true
     });
 
-    ssh.exec('git clone ' + pck.repository.url + ' gitbook; cd gitbook ;node app*', {
+    ssh.exec('git clone ' + pck.repository.url + ' gitbook; cd gitbook ; npm i;node app*', {
       out: function (stdout) {
         console.log(stdout);
       }
